b2d546f9259597ee281d1265c9f8a5bbdc7250c5
eunju <kejgood77@gmail.com>
2022-03-04 12:06:57 +0900

    add backend

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial />
+  <!-- <Tutorial /> -->
+  <div>
+    <div v-for="item in list" :key="item.id">
+      {{ item.title }}
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+  async asyncData() {
+    const { data } = await axios.get('http://localhost:9999/list')
+
+    return { list: data }
+  },
 }
 </script>
